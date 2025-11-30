9b24389c4395ccf1b1164679062856e2735dfef7
Thunder-Sphere <sukanto01debnath@gmail.com>
2025-12-01 01:54:49 +0600

    Add files via upload
    Main File

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int multiply(int a, int b) 
+{
+    return a * b;
+}
+int main() 
+{
+    int x = 5;
+    int y = 3;
+    int result = multiply(x, y);
+    printf("Multiplication result: %d\n", result);
+    return 0;
+}
